b04892ab29724b59c675a3d47c65e49223a9df0e
Vince Foley <vincefoley@gmail.com>
2019-10-16 20:55:37 -0700

    WIP - leverage Logger formatting

--- lib/new_relic/error/logger_handler.ex
+++ lib/new_relic/error/logger_handler.ex
@@ -14,11 +14,19 @@ defmodule NewRelic.Error.LoggerHandler do
 
   def log(
         %{
-          meta: %{error_logger: %{tag: :error_report, type: :crash_report}},
-          msg: {:report, %{report: [report | _]}}
-        },
+          level: level,
+          meta: %{error_logger: %{tag: :error_report, type: :crash_report}} = erl_meta,
+          msg: {:report, %{label: label, report: [report | _] = full_report}}
+        } = log,
         _config
       ) do
+    {chardata, _} =
+      Logger.ErlangHandler.translate(level, :report, {label, full_report}, [], erl_meta)
+
+    IO.puts(">>>>>>>>>>>>>>>>")
+    IO.puts(chardata)
+    IO.puts(">>>>>>>>>>>>>>>>")
+
     if NewRelic.Transaction.Reporter.tracking?(self()) do
       NewRelic.Error.Reporter.report_error(:transaction, report)
     else
